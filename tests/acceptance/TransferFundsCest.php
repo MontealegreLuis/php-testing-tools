2498a257454855401bd7f2029b766770c99705ed
Luis Montealegre <montealegreluis@gmail.com>
2015-07-23 09:53:52 -0500

    Generate Cest.

---
+++ tests/acceptance/TransferFundsCest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * PHP version 5.6
+ *
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+class TransferFundsCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}

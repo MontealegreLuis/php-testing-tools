f65b8b9b8d2a1c0cc4998332dc87ac74e6e501e4
Luis Montealegre <montealegreluis@gmail.com>
2015-07-23 10:05:28 -0500

    Add end-to-end test.

--- tests/acceptance/TransferFundsCest.php
+++ tests/acceptance/TransferFundsCest.php
@@ -4,18 +4,37 @@
  *
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
+use Page\TransferFundsPage;
+
 class TransferFundsCest
 {
-    public function _before(AcceptanceTester $I)
+    use \EwalletTestsBridge\ProvidesDoctrineSetup;
+
+    public function _before()
     {
+        $this->_setUpDoctrine();
+        $this
+            ->entityManager
+            ->createQuery('DELETE FROM ' . \Ewallet\Accounts\Member::class)
+            ->execute()
+        ;
+        \Nelmio\Alice\Fixtures::load(
+            __DIR__ . '/../_data/fixtures/members.yml', $this->entityManager
+        );
     }
 
-    public function _after(AcceptanceTester $I)
+    public function tryToTransferFundsBetweenMembers(AcceptanceTester $I)
     {
-    }
+        $I->am('ewallet member');
+        $I->wantTo('pay a debt');
+        $I->lookForwardTo('transfer funds to my friend');
 
-    // tests
-    public function tryToTest(AcceptanceTester $I)
-    {
+        $I->amOnPage(TransferFundsPage::$formPage);
+        $I->selectOption(TransferFundsPage::$toMember, 'Luis Montealegre');
+        $I->fillField(TransferFundsPage::$amount, 5);
+        $I->click(TransferFundsPage::$transfer);
+
+        $I->seeCurrentUrlEquals(TransferFundsPage::$transferPage);
+        $I->seeElement(TransferFundsPage::$successMessage);
     }
 }

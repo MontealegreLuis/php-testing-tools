603a8241ba78fb25a1fc00b9641d63ac1969d2ba
Luis Montealegre <montealegreluis@gmail.com>
2015-07-22 19:39:11 -0500

    Add 'Transfer funds' feature.

---
+++ features/bootstrap/MemberContext.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * PHP version 5.6
+ *
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Defines application features from the specific context.
+ */
+class MemberContext implements Context, SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
---
+++ features/member/transfer-funds.feature
@@ -0,0 +1,11 @@
+Feature: Transfer funds to another member
+  In order to send a gift or pay a debt
+  As a member
+  I want to be able to transfer funds to another member
+
+  Scenario: Make a transfer with sufficient funds
+    Given I have an account balance of "5000" MXN
+    And my friend has an account balance of "4000" MXN
+    When I transfer him "2000" MXN
+    Then my balance should be "3000" MXN
+    And my friend's balance should be "6000" MXN

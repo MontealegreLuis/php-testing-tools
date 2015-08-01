854d1bc7412bc945ac24f624afdf55bd87ff992c
Luis Montealegre <montealegreluis@gmail.com>
2015-08-01 09:28:37 -0500

    Add custom PHPUnit constraint.

---
+++ tests/EwalletTestsBridge/MoneyConstraint.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * PHP version 5.6
+ *
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+namespace EwalletTestsBridge;
+
+use Money\Money;
+use PHPUnit_Framework_Constraint as Constraint;
+
+class MoneyConstraint extends Constraint
+{
+    /** @var integer */
+    private $amountExpected;
+
+    /**
+     * @param integer $expected
+     */
+    public function __construct($expected)
+    {
+        $this->amountExpected = (integer) $expected;
+        parent::__construct();
+    }
+
+    /**
+     * @param mixed $other
+     * @return boolean
+     */
+    protected function matches($other)
+    {
+        /** @var Money $other */
+        return $this->amountExpected === $other->getAmount();
+    }
+
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return string
+     */
+    public function toString()
+    {
+        return 'has the correct amount';
+    }
+}
--- tests/integration/Ewallet/Wallet/TransferFundsTest.php
+++ tests/integration/Ewallet/Wallet/TransferFundsTest.php
@@ -10,13 +10,14 @@ use Ewallet\Accounts\Identifier;
 use Ewallet\Accounts\Member;
 use Ewallet\Accounts\Members;
 use EwalletTestsBridge\ProvidesDoctrineSetup;
+use EwalletTestsBridge\ProvidesMoneyConstraint;
 use Money\Money;
 use Nelmio\Alice\Fixtures;
 use PHPUnit_Framework_TestCase as TestCase;
 
 class TransferFundsTest extends TestCase
 {
-    use ProvidesDoctrineSetup;
+    use ProvidesDoctrineSetup, ProvidesMoneyConstraint;
 
     public function setUp()
     {
@@ -47,11 +48,7 @@ class TransferFundsTest extends TestCase
             Money::MXN(300)
         );
 
-        $this->assertEquals(
-            700, $result->fromMember()->accountBalance()->getAmount()
-        );
-        $this->assertEquals(
-            1300, $result->toMember()->accountBalance()->getAmount()
-        );
+        $this->assertBalanceAmounts(700, $result->fromMember()->accountBalance());
+        $this->assertBalanceAmounts(1300, $result->toMember()->accountBalance());
     }
 }

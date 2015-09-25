7c83f1ed3f6d1f9f30e31fa2ea338a2cae2d7130
Luis Montealegre <montealegreluis@gmail.com>
2015-09-25 15:38:36 -0500

    Improve error message in phpspec inline matcher.

--- tests/src/Ewallet/Bridges/Tests/ProvidesMoneyMatcher.php
+++ tests/src/Ewallet/Bridges/Tests/ProvidesMoneyMatcher.php
@@ -7,14 +7,25 @@
 namespace Ewallet\Bridges\Tests;
 
 use Money\Money;
+use PhpSpec\Exception\Example\FailureException;
 
+/**
+ * Adds `shouldAmount` inline matcher to verify that an amount expressed in
+ * cents is equal to the amount of the subject `Money` object
+ */
 trait ProvidesMoneyMatcher
 {
     public function getMatchers()
     {
         return [
-            'amount' => function(Money $subject, $value) {
-                return $subject->getAmount() === $value;
+            'amount' => function(Money $subject, $expectedAmount) {
+                if ($subject->getAmount() !== $expectedAmount) {
+                    throw new FailureException(sprintf(
+                        'Current money amount should be "%s" not "%s".',
+                        $expectedAmount, $subject->getAmount()
+                    ));
+                }
+                return true;
             }
         ];
     }

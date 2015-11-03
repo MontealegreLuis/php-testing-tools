8b0a7b38be58f08794862e805308a2dfca2aa908
Luis Montealegre <montealegreluis@gmail.com>
2015-11-03 16:51:06 -0600

    Add command to notify domain events through messaging.

---
+++ tests/integration/EwalletApplication/Bridges/SymfonyConsole/Commands/PublishMessagesCommandTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * PHP version 5.6
+ *
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+namespace EwalletApplication\Bridges\SymfonyConsole\Commands;
+
+use Hexagonal\Messaging\MessagePublisher;
+use Mockery;
+use PHPUnit_Framework_TestCase as TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class PublishMessagesCommandTest extends TestCase
+{
+    /** @test */
+    function it_should_return_count_of_published_messages()
+    {
+        $publisher = Mockery::mock(MessagePublisher::class);
+        $publisher
+            ->shouldReceive('publishTo')
+            ->once()
+            ->with('ewallet')
+            ->andReturn(3)
+        ;
+
+        $tester = new CommandTester(new PublishMessagesCommand($publisher));
+        $tester->execute([
+            'exchange' => 'ewallet',
+        ]);
+
+        $this->assertRegexp(
+            '/3 messages published/',
+            $tester->getDisplay()
+        );
+    }
+}

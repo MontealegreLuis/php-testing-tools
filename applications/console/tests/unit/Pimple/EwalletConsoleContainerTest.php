ca5aba9673bdf59e62764fa7b9ce0c74abc1d54d
Luis Montealegre <montealegreluis@gmail.com>
2016-06-18 21:28:25 -0500

    Only keep assertions for objects defined in the service provider for the console.

--- applications/console/tests/unit/Pimple/EwalletConsoleContainerTest.php
+++ applications/console/tests/unit/Pimple/EwalletConsoleContainerTest.php
@@ -6,60 +6,24 @@
  */
 namespace Ewallet\Pimple;
 
-use Doctrine\ORM\EntityManager;
 use Dotenv\Dotenv;
-use Ewallet\Doctrine2\Accounts\DoctrineMembers;
-use Ewallet\Wallet\TransferFundsTransactionally;
 use Ewallet\Actions\TransferFundsAction;
-use Ewallet\EasyForms\MembersConfiguration;
-use Ewallet\Listeners\LogTransferWasMadeSubscriber;
 use Ewallet\Responders\TransferFundsConsoleResponder;
-use Ewallet\Zf2\InputFilter\TransferFundsInputFilterRequest;
-use Ewallet\Presenters\MemberFormatter;
-use Hexagonal\Doctrine2\DomainEvents\EventStoreRepository;
-use Hexagonal\Doctrine2\Messaging\MessageTrackerRepository;
+use Ewallet\SymfonyConsole\Listeners\StoreEventsListener;
 use Hexagonal\DomainEvents\EventPublisher;
-use Hexagonal\Messaging\MessagePublisher;
-use Monolog\Logger;
 use PHPUnit_Framework_TestCase as TestCase;
-use Symfony\Component\Console\Input\ArgvInput;
-use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\{Input\ArgvInput, Output\ConsoleOutput};
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class EwalletConsoleContainerTest extends TestCase
 {
     /** @test */
-    function it_should_create_the_console_application_services()
+    function it_creates_the_console_application_services()
     {
+        (new Dotenv(__DIR__ . '/../../../', '.env.tests'))->load();
         $options = require __DIR__ . '/../../../config.tests.php';
         $container = new EwalletConsoleContainer($options);
 
-        $this->assertInstanceOf(
-            EntityManager::class,
-            $container['doctrine.em']
-        );
-        $this->assertInstanceOf(
-            DoctrineMembers::class,
-            $container['ewallet.member_repository']
-        );
-        /*
-        $this->assertInstanceOf(
-            MembersConfiguration::class,
-            $container['ewallet.members_configuration']
-        );
-        */
-        $this->assertInstanceOf(
-            MemberFormatter::class,
-            $container['ewallet.member_formatter']
-        );
-        $this->assertInstanceOf(
-            TransferFundsInputFilterRequest::class,
-            $container['ewallet.transfer_filter_request']
-        );
-        $this->assertInstanceOf(
-            TransferFundsTransactionally::class,
-            $container['ewallet.transfer_funds']
-        );
-        // console objects: 3
         $this->assertInstanceOf(
             ArgvInput::class,
             $container['ewallet.console_input']
@@ -77,51 +41,16 @@ class EwalletConsoleContainerTest extends TestCase
             $container['ewallet.transfer_funds_console_action']
         );
         $this->assertInstanceOf(
-            Logger::class,
-            $container['ewallet.logger']
-        );
-        $this->assertInstanceOf(
-            LogTransferWasMadeSubscriber::class,
-            $container['ewallet.transfer_funds_logger']
-        );
-        /*
-        $this->assertInstanceOf(
-            TransferFundsEmailNotifier::class,
-            $container['ewallet.transfer_mail_notifier']
-        );
-        */
-        $this->assertInstanceOf(
             EventPublisher::class,
             $container['ewallet.events_publisher']
         );
-        /*
         $this->assertInstanceOf(
-            EventStoreRepository::class,
-            $container['hexagonal.event_store_repository']
+            StoreEventsListener::class,
+            $container['ewallet.store_events_listener']
         );
         $this->assertInstanceOf(
-            MessageTrackerRepository::class,
-            $container['hexagonal.message_tracker_repository']
+            EventDispatcher::class,
+            $container['ewallet.console.dispatcher']
         );
-        $this->assertInstanceOf(
-            AMQPStreamConnection::class,
-            $connection = $container['hexagonal.amqp_connection']
-        );
-        $connection->close();
-        $this->assertInstanceOf(
-            AmqpMessageProducer::class,
-            $producer = $container['hexagonal.messages_producer']
-        );
-        $producer->close();
-        $this->assertInstanceOf(
-            AmqpMessageConsumer::class,
-            $consumer = $container['hexagonal.messages_consumer']
-        );
-        $consumer->close();
-        $this->assertInstanceOf(
-            MessagePublisher::class,
-            $container['hexagonal.messages_publisher']
-        );
-        */
     }
 }

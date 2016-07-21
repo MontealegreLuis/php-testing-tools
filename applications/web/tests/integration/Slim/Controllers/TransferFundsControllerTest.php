25fd3931906a0aae4cb482057ab566bd29891d9c
Luis Montealegre <montealegreluis@gmail.com>
2016-07-21 13:34:32 -0500

    Extract setup code from Slim smoke test.

--- applications/web/tests/integration/Slim/Controllers/TransferFundsControllerTest.php
+++ applications/web/tests/integration/Slim/Controllers/TransferFundsControllerTest.php
@@ -7,20 +7,33 @@
 namespace Ewallet\Slim\Controllers;
 
 use Ewallet\Slim\Application;
+use Interop\Container\ContainerInterface;
 use PHPUnit_Framework_TestCase as TestCase;
 use Slim\Http\Environment;
 use Slim\Http\Request;
 
 class TransferFundsControllerTest extends TestCase
 {
+    /** @var  Application */
+    private $app;
+
+    /** @var ContainerInterface */
+    private $container;
+
+    /** @before */
+    public function configureApplication()
+    {
+        $this->app = new Application(require __DIR__ . '/../../../../config.tests.php');
+        $this->container = $this->app->getContainer();
+    }
+
     /** @test */
     function it_returns_an_ok_response_on_enter_transfer_information_action()
     {
-        $app = new Application(require __DIR__ . '/../../../../config.tests.php');
-        $app->getContainer()['environment'] = Environment::mock([
+        $this->container['environment'] = Environment::mock([
             'REQUEST_URI' => '/transfer-form',
         ]);
-        $response = $app->run(true);
+        $response = $this->app->run(true);
 
         $this->assertEquals(200, $response->getStatusCode());
     }
@@ -28,21 +41,20 @@ class TransferFundsControllerTest extends TestCase
     /** @test */
     function it_returns_an_ok_response_on_transfer_funds_action()
     {
-        $app = new Application(require __DIR__ . '/../../../../config.tests.php');
-        $environment = Environment::mock([
+        $this->container['environment'] = Environment::mock([
             'REQUEST_URI' => '/transfer-funds',
             'REQUEST_METHOD' => 'POST',
             'CONTENT_TYPE' => 'application/x-www-form-urlencoded',
         ]);
-        $app->getContainer()['environment'] = $environment;
-        $request = Request::createFromEnvironment($environment);
+        $request = Request::createFromEnvironment($this->container['environment']);
         $request = $request->withParsedBody([
             'senderId' => 'BCD',
             'recipientId' => 'EFG',
             'amount' => 2,
         ]);
-        $app->getContainer()['request'] = $request;
-        $response = $app->run(true);
+        $this->container['request'] = $request;
+
+        $response = $this->app->run(true);
 
         $this->assertEquals(200, $response->getStatusCode());
     }

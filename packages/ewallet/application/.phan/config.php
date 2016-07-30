8484fbd6eb24ede8d776212947c1f936f189e66d
Luis Montealegre <montealegreluis@gmail.com>
2016-07-30 13:22:24 -0500

    Add Phan configuration to ewallet/application

---
+++ packages/ewallet/application/.phan/config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    "directory_list" => [
+        __DIR__ . "/../src",
+        __DIR__ . '/../vendor/ewallet/domain',
+        __DIR__ . '/../vendor/hexagonal/hexagonal',
+        __DIR__ . '/../vendor/mathiasverraes/money',
+        __DIR__ . '/../vendor/psr/log',
+    ],
+    "exclude_analysis_directory_list" => [
+        __DIR__ . '/../vendor',
+    ],
+];

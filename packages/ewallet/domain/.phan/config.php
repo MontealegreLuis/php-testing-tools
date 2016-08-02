9f1b21fb220cc7008ea4018f7257387116175127
Luis Montealegre <montealegreluis@gmail.com>
2016-08-01 21:11:42 -0500

    Add Phan configuration to package ewallet/domain.

---
+++ packages/ewallet/domain/.phan/config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    "directory_list" => [
+        __DIR__ . "/../src",
+        __DIR__ . '/../vendor/beberlei/assert',
+        __DIR__ . '/../vendor/hexagonal/hexagonal',
+        __DIR__ . '/../vendor/mathiasverraes/money',
+    ],
+    "exclude_analysis_directory_list" => [
+        __DIR__ . '/../vendor',
+    ],
+];

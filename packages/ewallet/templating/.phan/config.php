b2665aee66f9d0ee0c56932c3d850bd311548514
Luis Montealegre <montealegreluis@gmail.com>
2016-08-03 21:26:14 -0500

    Add Phan configuration to package ewallet/templating.

---
+++ packages/ewallet/templating/.phan/config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    "directory_list" => [
+        __DIR__ . "/../src",
+        __DIR__ . '/../vendor/ewallet/application',
+        __DIR__ . '/../vendor/mathiasverraes/money',
+        __DIR__ . '/../vendor/twig/twig',
+    ],
+    "exclude_analysis_directory_list" => [
+        __DIR__ . '/../vendor',
+    ],
+];

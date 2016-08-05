1bdc66bd8cb9043fb130842f29e12567701e73f9
Luis Montealegre <montealegreluis@gmail.com>
2016-08-04 20:11:16 -0500

    Add Phan configuration to package ewallet/validation.

---
+++ packages/ewallet/validation/.phan/config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    "directory_list" => [
+        __DIR__ . "/../src",
+        __DIR__ . '/../vendor/ewallet/application',
+        __DIR__ . '/../vendor/ewallet/domain',
+        __DIR__ . '/../vendor/zendframework/zend-inputfilter',
+        __DIR__ . '/../vendor/zendframework/zend-validator',
+    ],
+    "exclude_analysis_directory_list" => [
+        __DIR__ . '/../vendor',
+    ],
+];

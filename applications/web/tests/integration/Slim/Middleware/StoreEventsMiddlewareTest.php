4ce6688c481973b3fce4b9d49065f2bc49a2d892
Luis Montealegre <montealegreluis@gmail.com>
2017-01-13 22:30:57 -0600

    Cleanup test case. Configuration code is now in the test setup.

--- applications/web/tests/integration/Slim/Middleware/StoreEventsMiddlewareTest.php
+++ applications/web/tests/integration/Slim/Middleware/StoreEventsMiddlewareTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * PHP version 7.0
+ * PHP version 7.1
  *
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
@@ -18,37 +18,17 @@ class StoreEventsMiddlewareTest extends TestCase
 {
     use ProvidesDoctrineSetup;
 
-    /** @before */
-    function cleanUpEvents()
-    {
-        $this->_setUpDoctrine(require __DIR__ . '/../../../../config.php');
-        $this
-            ->_entityManager()
-            ->createQuery('DELETE FROM ' . StoredEvent::class)
-            ->execute()
-        ;
-    }
-
     /** @test */
     function it_persists_an_event_published_inside_an_slim_route()
     {
-        /** @var \Hexagonal\Doctrine2\DomainEvents\EventStoreRepository $store */
-        $store = $this->_entityManager()->getRepository(StoredEvent::class);
-
-        $publisher = new EventPublisher();
-        $middleware = new StoreEventsMiddleware(
-            new PersistEventsSubscriber(
-                $store, new StoredEventFactory(new JsonSerializer())
-            ),
-            $publisher
-        );
-
         $app = new App();
         $container = $app->getContainer();
-        $container['environment'] = Environment::mock([
-            'REQUEST_URI' => '/',
-        ]);
-        $app->add($middleware);
+        $container['environment'] = Environment::mock(['REQUEST_URI' => '/']);
+        $app->add($this->middleware);
+
+        // If you use the instance variable it will try to use the container
+        // instead of the instance defined by this class
+        $publisher = $this->publisher;
         $app->get('/', function($_, $response) use ($publisher) {
             $events = new SplObjectStorage();
             $events->attach(A::transferWasMadeEvent()->build());
@@ -56,9 +36,40 @@ class StoreEventsMiddlewareTest extends TestCase
 
             return $response;
         })->setName('home');
+
         $response = $app->run(true);
 
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertCount(1, $store->allEvents());
+        $this->assertCount(1, $this->store->allEvents());
     }
+
+    /** @before */
+    function configureMiddleware()
+    {
+        $this->_setUpDoctrine(require __DIR__ . '/../../../../config.php');
+        $this
+            ->_entityManager()
+            ->createQuery('DELETE FROM ' . StoredEvent::class)
+            ->execute()
+        ;
+
+        /** @var \Hexagonal\Doctrine2\DomainEvents\EventStoreRepository $store */
+        $this->store = $this->_repositoryForEntity(StoredEvent::class);
+        $this->publisher = new EventPublisher();
+        $this->middleware = new StoreEventsMiddleware(
+            new PersistEventsSubscriber(
+                $this->store, new StoredEventFactory(new JsonSerializer())
+            ),
+            $this->publisher
+        );
+    }
+
+    /** @var  StoreEventsMiddleware */
+    private $middleware;
+
+    /** @var  EventPublisher */
+    private $publisher;
+
+    /** @var \Hexagonal\Doctrine2\DomainEvents\EventStoreRepository */
+    private $store;
 }

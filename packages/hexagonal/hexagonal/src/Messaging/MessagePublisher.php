2a302c1adc049a73c792d654ab4ee2b1741bd10b
Luis Montealegre <montealegreluis@gmail.com>
2017-01-30 16:01:28 -0600

    Refactor message publisher.

--- packages/hexagonal/hexagonal/src/Messaging/MessagePublisher.php
+++ packages/hexagonal/hexagonal/src/Messaging/MessagePublisher.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * PHP version 7.0
+ * PHP version 7.1
  *
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
@@ -11,20 +11,29 @@ use Hexagonal\DomainEvents\EventStore;
 
 class MessagePublisher
 {
+    private const NO_MESSAGES_PUBLISHED = 0;
+
     /** @var EventStore */
     private $store;
 
-    /** @var  MessageTracker */
+    /** @var MessageTracker */
     private $tracker;
 
     /** @var MessageProducer */
     private $producer;
 
-    /**
-     * @param EventStore $store
-     * @param MessageTracker $tracker
-     * @param MessageProducer $producer
-     */
+    /** @var PublishedMessage */
+    private $mostRecentMessage;
+
+    /** @var \Hexagonal\DomainEvents\StoredEvent[] */
+    private $unpublishedEvents;
+
+    /** @var int */
+    private $publishedMessagesCount;
+
+    /** @var \Hexagonal\DomainEvents\StoredEvent */
+    private $lastPublishedEvent;
+
     public function __construct(
         EventStore $store,
         MessageTracker $tracker,
@@ -36,49 +45,98 @@ class MessagePublisher
     }
 
     /**
-     * @param string $exchangeName
-     * @return int
+     * @return int The amount of messages that were published
+     * @throws \Hexagonal\Messaging\InvalidPublishedMessageToTrack
+     * @throws \Hexagonal\Messaging\EmptyExchange
      */
     public function publishTo(string $exchangeName): int
     {
-        if (!$this->tracker->hasPublishedMessages($exchangeName)) {
-            $mostRecentMessage = null;
-            $messages = $this->store->allEvents();
+        $this->startMessageTracking();
+
+        if ($this->tracker->hasPublishedMessages($exchangeName)) {
+            $this->onlyUnpublishedEvents($exchangeName);
         } else {
-            $mostRecentMessage = $this->tracker->mostRecentPublishedMessage($exchangeName);
-            $messages = $this->store->eventsStoredAfter($mostRecentMessage->mostRecentMessageId());
+            $this->allEvents();
         }
 
-        if (!$messages) {
-            return 0;
+        if ($this->nothingToPublish()) {
+            return self::NO_MESSAGES_PUBLISHED;
         }
-
-        $publishedMessages = 0;
-        $lastPublishedNotification = null;
 
         try {
-            $this->producer->open($exchangeName);
-            foreach ($messages as $message) {
-                $this->producer->send($exchangeName, $message);
-                $lastPublishedNotification = $message;
-                $publishedMessages++;
-            }
-            $this->producer->close();
+            $this->publish($exchangeName);
         } catch (Exception $ignore) {
-        } finally {
-            if (!$mostRecentMessage) {
-                $mostRecentMessage = new PublishedMessage(
-                    $exchangeName,
-                    $lastPublishedNotification->id()
-                );
-            } else {
-                $mostRecentMessage->updateMostRecentMessageId(
-                    $lastPublishedNotification->id()
-                );
-            }
-            $this->tracker->track($mostRecentMessage);
+            /* Ignore any exception produced by any consumer */
         }
 
-        return $publishedMessages;
+        if (!$this->mostRecentMessage) {
+            $this->createMostRecentMessage($exchangeName);
+        } else {
+            $this->updateMostRecentMessage();
+        }
+
+        $this->tracker->track($this->mostRecentMessage);
+
+        return $this->publishedMessagesCount;
+    }
+
+    private function allEvents()
+    {
+        $this->mostRecentMessage = null;
+        $this->unpublishedEvents = $this->store->allEvents();
+    }
+
+    /**
+     * @param string $exchangeName
+     * @throws \Hexagonal\Messaging\EmptyExchange
+     */
+    private function onlyUnpublishedEvents(string $exchangeName)
+    {
+        $this->mostRecentMessage = $this->tracker->mostRecentPublishedMessage(
+            $exchangeName
+        );
+        $this->unpublishedEvents = $this->store->eventsStoredAfter(
+            $this->mostRecentMessage->mostRecentMessageId()
+        );
+    }
+
+    private function publish(string $exchangeName)
+    {
+        $this->producer->open($exchangeName);
+
+        /** @var \Hexagonal\DomainEvents\StoredEvent $message */
+        foreach ($this->unpublishedEvents as $message) {
+            $this->producer->send($exchangeName, $message);
+            $this->lastPublishedEvent = $message;
+            $this->publishedMessagesCount++;
+        }
+
+        $this->producer->close();
+    }
+
+    private function createMostRecentMessage(string $exchangeName)
+    {
+        $this->mostRecentMessage = new PublishedMessage(
+            $exchangeName,
+            $this->lastPublishedEvent->id()
+        );
+    }
+
+    private function updateMostRecentMessage()
+    {
+        $this->mostRecentMessage->updateMostRecentMessageId(
+            $this->lastPublishedEvent->id()
+        );
+    }
+
+    private function startMessageTracking()
+    {
+        $this->publishedMessagesCount = self::NO_MESSAGES_PUBLISHED;
+        $this->lastPublishedEvent = null;
+    }
+
+    private function nothingToPublish(): bool
+    {
+        return empty($this->unpublishedEvents);
     }
 }
